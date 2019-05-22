2a7b6594bbe4e31340733e2aac9d15f2811536f8
lowie spriet <lowie.spriet@gmail.com>
2019-05-22 20:47:21 +0200

    mqtt connectie werkt zonder recive

---
+++ src/Hololens/HORO_project/HolographicAcademy-Holograms-101/HORO_project/Assets/M2MqttUnity/Examples/Scripts/Emyl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using M2MqttUnity;
+
+namespace M2MqttUnity.Examples
+{
+    public class Emyl : M2MqttUnityClient
+    {
+        Boolean send = false;
+        // Start is called before the first frame update
+
+        protected override void OnConnected()
+        {
+            base.OnConnected();
+            send = true;
+            Debug.Log("connected");
+
+        }
+        protected override void Start()
+        {
+            Debug.Log("start is begonnen");
+            brokerAddress = "192.168.0.69";
+            brokerPort = 1883;
+            autoConnect = true;
+            base.Start();
+            Debug.Log("start is uitgevoerd");
+        }
+        protected override void DecodeMessage(string topic, byte[] message)
+        {
+            Debug.LogFormat("Message received on topic: {0}", topic);
+            string msg = System.Text.Encoding.UTF8.GetString(message);
+            Debug.Log("Received: " + msg);
+
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+           
+
+        }
+
+        public class Coordinaten
+        {
+            public int X;
+            public int Y;
+        }
+    }
+}
